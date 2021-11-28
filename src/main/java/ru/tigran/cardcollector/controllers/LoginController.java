7b99c287eeff254dd8dc27c668d065ce3d7fabeb
Tigran Shaginyan <68780335+TigranAI@users.noreply.github.com>
2021-11-28 07:15:11 +0300

    Move login link to java method

--- src/main/java/ru/tigran/cardcollector/controllers/LoginController.java
+++ src/main/java/ru/tigran/cardcollector/controllers/LoginController.java
@@ -1,18 +1,25 @@
 package ru.tigran.cardcollector.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import ru.tigran.cardcollector.Config;
 import ru.tigran.cardcollector.database.repository.SessionTokenRepository;
 import ru.tigran.cardcollector.database.repository.UserRepository;
+
+import java.net.InetAddress;
 
 @Controller
 @RequestMapping("/login")
 @SessionAttributes(names = {"user"})
 public class LoginController {
+    @Autowired
+    Environment environment;
 
     @Autowired
     private UserRepository userRepository;
@@ -29,5 +36,15 @@ public class LoginController {
         } catch (Exception e) {
             return "redirect:/";
         }
+    }
+
+    @GetMapping()
+    public String login(Model model){
+        String host = String.format("%s:%s",
+                InetAddress.getLoopbackAddress().getHostName(),
+                environment.getProperty("server.port"));
+        String botName = Config.get("telegram.bot.name");
+        String url = String.format("https://t.me/%s?start=create_token=%s", botName, host);
+        return "redirect:" + url;
     }
 }
