99a3e2b23139c8c993325849b5e62a4ac09b0447
Tigran <abvgdesh3@gmail.com>
2022-03-12 06:53:24 +0300

    bug fix

--- src/main/java/ru/tigran/cardcollector/WebConfigurer.java
+++ src/main/java/ru/tigran/cardcollector/WebConfigurer.java
@@ -2,13 +2,9 @@ package ru.tigran.cardcollector;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
-import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import javax.servlet.SessionTrackingMode;
-import java.util.HashSet;
 
 @Component
 public class WebConfigurer {
@@ -27,18 +23,6 @@ public class WebConfigurer {
                 registry.addMapping("/database/logs");
                 registry.addMapping("/sticker");
                 registry.addMapping("/collection");
-            }
-        };
-    }
-
-    @Bean
-    public WebApplicationInitializer webApplicationInitializer(){
-        boolean httpsEnable = Boolean.parseBoolean(Config.get("https.enable"));
-        return servletContext -> {
-            if (!httpsEnable){
-                HashSet<SessionTrackingMode> set = new HashSet<>();
-                set.add(SessionTrackingMode.COOKIE);
-                servletContext.setSessionTrackingModes(set);
             }
         };
     }
