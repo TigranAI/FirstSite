28ddb7349a836bf91ad55f7f63a3518d9714e176
Tigran <abvgdesh3@gmail.com>
2022-03-12 06:04:07 +0300

    fix resource loading on http

--- src/main/java/ru/tigran/cardcollector/WebConfigurer.java
+++ src/main/java/ru/tigran/cardcollector/WebConfigurer.java
@@ -2,14 +2,19 @@ package ru.tigran.cardcollector;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.SessionTrackingMode;
+import java.util.HashSet;
 
 @Component
 public class WebConfigurer {
     @Bean
     public WebMvcConfigurer webMvcConfigurerAdapter() {
+
         return new WebMvcConfigurer() {
             @Override
             public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -22,6 +27,18 @@ public class WebConfigurer {
                 registry.addMapping("/database/logs");
                 registry.addMapping("/sticker");
                 registry.addMapping("/collection");
+            }
+        };
+    }
+
+    @Bean
+    public WebApplicationInitializer webApplicationInitializer(){
+        boolean httpsEnable = Boolean.parseBoolean(Config.get("https.enable"));
+        return servletContext -> {
+            if (!httpsEnable){
+                HashSet<SessionTrackingMode> set = new HashSet<>();
+                set.add(SessionTrackingMode.COOKIE);
+                servletContext.setSessionTrackingModes(set);
             }
         };
     }
